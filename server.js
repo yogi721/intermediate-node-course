7381feb57958881055ac19db56438a2e6845f9ad
Mehdi <semlali.mehdi.1980@gmail.com>
2020-03-05 10:28:48 +0100

    add user model and connect to db

--- server.js
+++ server.js
@@ -4,6 +4,26 @@ const bodyParser = require('body-parser');
 const port = 8085;
 const app = express();
 
+const User = require('./models/User');
+const {
+  MONGO_USERNAME,
+  MONGO_PASSWORD,
+  MONGO_HOSTNAME,
+  MONGO_PORT,
+  MONGO_DB
+} = process.env;
+
+const options = {
+  useNewUrlParser: true,
+  reconnectTries: Number.MAX_VALUE,
+  reconnectInterval: 500,
+  connectTimeoutMS: 10000,
+  useCreateIndex: true,
+  useFindAndModify: false
+};
+const url = `mongodb://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOSTNAME}:${MONGO_PORT}/${MONGO_DB}?authSource=admin`;
+mongoose.connect(url, options);
+
 app.use(bodyParser.json());
 
 app.listen(port, () => {
